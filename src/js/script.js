bf1697d7c5f852dafc1a268abe04a0734a4db84f
Savianok Marta <magnolia.sav@gmail.com>
2024-07-08 18:00:55 +0300

    end

--- src/js/script.js
+++ src/js/script.js
@@ -23,3 +23,93 @@ closeMenuBtn?.addEventListener('click' , ()=> {
 overlay?.addEventListener('click' , ()=> {
     hideMenuModal()
 })
+
+
+const projectsSwiper = document.querySelector('.projects-swiper');
+
+if (projectsSwiper) {
+    const swiper1 = new Swiper(projectsSwiper, {
+        loop: true,
+        slidesPerView:1.3,
+        spaceBetween:20,
+        breakpoints: {
+            450: {
+                slidesPerView: 1.5,
+                spaceBetween: 20
+            },
+            630: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            768: {
+                slidesPerView:2.5,
+                spaceBetween: 20
+            },
+            900: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            },
+            1024: {
+                slidesPerView: 3.3,
+                spaceBetween: 20
+            },
+            1200: {
+                slidesPerView: 4,
+                spaceBetween: 30
+            }
+        },
+        scrollbar: {
+            el: '.project-swiper-scrollbar',
+            clickable: true
+        },
+    });
+}
+
+const showModal = (modal) => {
+    overlay.classList.add('open');
+    modal.classList.add('active');
+}
+const closeModal = (modal) => {
+    overlay.addEventListener('click' , () => {
+        overlay.classList.remove('open');
+        modal.classList.remove('active');
+    })
+    const closeBtns =  modal.querySelectorAll('.close-btn');
+    closeBtns?.forEach(closeBtn => {
+        closeBtn.addEventListener('click' , () => {
+            overlay.classList.remove('open');
+            modal.classList.remove('active');
+        })
+    })
+
+}
+
+const documentsModal = document.querySelector('.documents-modal');
+const newsSingleModal = document.querySelector('.news-single-modal');
+const contactsModal = document.querySelector('.contacts-modal');
+const showNewsSingleModalBtns = document.querySelectorAll('.news-card');
+const showContactsModalBtns =  document.querySelectorAll('.show-contacts-modal');
+const showDocumentsModalBtns =  document.querySelectorAll('.show-documents-modal');
+
+showNewsSingleModalBtns?.forEach(el=> {
+    el.addEventListener('click' , (e) => {
+        e.preventDefault();
+        showModal(newsSingleModal);
+        closeModal(newsSingleModal);
+    })
+})
+showContactsModalBtns?.forEach(el=> {
+    el.addEventListener('click' , (e) => {
+        e.preventDefault();
+        showModal(contactsModal);
+        closeModal(contactsModal);
+    })
+})
+
+showDocumentsModalBtns?.forEach(el=> {
+    el.addEventListener('click' , (e) => {
+        e.preventDefault();
+        showModal(documentsModal);
+        closeModal(documentsModal);
+    })
+})
